12a08f5d2a01b8192dc7d67acd4d3ec06b9fd77e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-07 20:07:09 +0900

    [ Feat ] 경로 line

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./Main.Styles.css";
-import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
+import { Map, Polyline, useKakaoLoader } from "react-kakao-maps-sdk";
+
+interface MarkerPoint {
+  lat: number;
+  lng: number;
+}
 
 const Main = () => {
   const [position, setPosition] = useState({ lat: 33.450701, lng: 126.570667 });
+  const [path, setPath] = useState<MarkerPoint[]>([]);
   const { VITE_APP_TITLE } = import.meta.env;
 
   // 해당 함수가 무조건 한번 실행이 되어야 웹 자체에서 카카오 맵 API의 구동이 정상적으로 진행될 수 있습니다.
@@ -25,11 +31,24 @@ const Main = () => {
       lat: coords.latitude,
       lng: coords.longitude,
     });
+
+    setPath((prev) => [
+      ...prev,
+      { lat: coords.latitude, lng: coords.longitude },
+    ]);
   });
 
   return (
     <article className="main__layout">
-      <Map className="main__map" center={position} level={3} />
+      <Map className="main__map" center={position} level={3}>
+        <Polyline
+          path={path}
+          strokeWeight={5}
+          strokeOpacity={0.7}
+          strokeColor={"#ff7f50"}
+          strokeStyle={"solid"}
+        />
+      </Map>
     </article>
   );
 };
