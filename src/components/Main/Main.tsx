0540369fb91278457b8629ba2a0f5f3ced6317d9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-07 20:12:07 +0900

    [ Feat ] Count 구현

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,6 +10,7 @@ interface MarkerPoint {
 const Main = () => {
   const [position, setPosition] = useState({ lat: 33.450701, lng: 126.570667 });
   const [path, setPath] = useState<MarkerPoint[]>([]);
+  const [fetchCount, setFetchCount] = useState(0);
   const { VITE_APP_TITLE } = import.meta.env;
 
   // 해당 함수가 무조건 한번 실행이 되어야 웹 자체에서 카카오 맵 API의 구동이 정상적으로 진행될 수 있습니다.
@@ -36,20 +37,25 @@ const Main = () => {
       ...prev,
       { lat: coords.latitude, lng: coords.longitude },
     ]);
+
+    setFetchCount((count) => count + 1);
   });
 
   return (
-    <article className="main__layout">
-      <Map className="main__map" center={position} level={3}>
-        <Polyline
-          path={path}
-          strokeWeight={5}
-          strokeOpacity={0.7}
-          strokeColor={"#ff7f50"}
-          strokeStyle={"solid"}
-        />
-      </Map>
-    </article>
+    <>
+      <h1 className="count">{fetchCount}</h1>
+      <article className="main__layout">
+        <Map className="main__map" center={position} level={3}>
+          <Polyline
+            path={path}
+            strokeWeight={5}
+            strokeOpacity={0.7}
+            strokeColor={"#ff7f50"}
+            strokeStyle={"solid"}
+          />
+        </Map>
+      </article>
+    </>
   );
 };
 
