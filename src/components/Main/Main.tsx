f0254e09b1f8a87e74987e432ad2189b717a4d0b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-07 19:57:11 +0900

    [ feat ] gps 테스트

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./Main.Styles.css";
 import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
 
 const Main = () => {
+  const [position, setPosition] = useState({ lat: 33.450701, lng: 126.570667 });
   const { VITE_APP_TITLE } = import.meta.env;
 
   // 해당 함수가 무조건 한번 실행이 되어야 웹 자체에서 카카오 맵 API의 구동이 정상적으로 진행될 수 있습니다.
@@ -11,13 +13,23 @@ const Main = () => {
     libraries: ["clusterer", "drawing", "services"],
   });
 
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    setPosition({
+      lat: coords.latitude,
+      lng: coords.longitude,
+    });
+  });
+
+  navigator.geolocation.watchPosition(({ coords }) => {
+    setPosition({
+      lat: coords.latitude,
+      lng: coords.longitude,
+    });
+  });
+
   return (
     <article className="main__layout">
-      <Map
-        className="main__map"
-        center={{ lat: 33.450701, lng: 126.570667 }}
-        level={3}
-      />
+      <Map className="main__map" center={position} level={3} />
     </article>
   );
 };
